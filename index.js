77af5cc6c48cd90add7ef13111128b9fb75d72b8
Gum Joe <kishansambhi@hotmail.co.uk>
2017-02-14 12:34:41 +0000

    :book: Added programming page :book: Added programming page and started adding content

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 // ES6
-/**
- * Animate sidebar
- * Using jquery
- * Actual animation is done in css
- */
-$(".hamburger").click(() => {
-  $(".sidebar").appendClass("sidebar-animate");
-  $(".main").animate({opacity: 0.5}, 10000);
-});
+// Vars
+const SIDEBAR_IN_PROPS = {
+    sidebar: {
+        marginLeft: 0
+    },
+    body: {
+        opacity: 0.5
+    }
+};
+
+const SIDEBAR_OUT_PROPS = {
+    sidebar: {
+        marginLeft: "-150px"
+    },
+    body: {
+        opacity: 1
+    }
+};
+        
+        /**
+         * Animate sidebar
+         * Using jquery
+         * Generally, I would use css classes and a css animation to open the sidebar
+         * With JQuery attaching the classes
+         * But I am doing it this way so I have JS in my page
+         */
+        $(".hamburger").click(() => {
+          $(".sidebar").animate(SIDEBAR_IN_PROPS.sidebar, 500);
+          $(".main").animate(SIDEBAR_IN_PROPS.body, 500);
+        });
+
+        $("#sidebar-head").click(() => {
+          $(".sidebar").animate(SIDEBAR_OUT_PROPS.sidebar, 500);
+          $(".main").animate(SIDEBAR_OUT_PROPS.body, 500);
+        });
